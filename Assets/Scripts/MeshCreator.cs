f3c0fd515034cb75c48354eec949435918b07065
Nishchhal Bakshi <nishchhal.bakshi@iicorporate.com>
2019-07-23 21:03:50 +0530

    Initial commit

---
+++ Assets/Scripts/MeshCreator.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MeshCreator : MonoBehaviour
+{
+    public int vertexCount;
+    public float bottomPoint;
+    public float Xspacing;
+    public float Yspacing;
+    public int verticalVertexCount = 2;
+
+    public float scale = .1f;
+    public static float seed;
+    public float height;
+
+    private int currentVerticalVertexCount;
+    public float xValue;
+    [SerializeField] private List<Vector3> vertexList = new List<Vector3>();
+    [SerializeField] private List<int> triangleList = new List<int>();
+    EdgeCollider2D edgeCollider2D;
+
+    private Mesh myMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.sharedMesh = CreateMesh();
+        myMesh = meshFilter.sharedMesh;
+        edgeCollider2D = gameObject.AddComponent<EdgeCollider2D>();
+
+        //yield return new WaitForSecondsRealtime(2f);
+        ManipulateMesh(myMesh);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+
+        xValue = bottomPoint;
+        int[] trianglesArray;
+
+        for (int i = 0; i < vertexCount; i++)
+        {
+            if(currentVerticalVertexCount == verticalVertexCount)
+            {
+                currentVerticalVertexCount = 0;
+                xValue += Xspacing;
+            }
+
+            Vector3 vertex;
+            vertex = new Vector3(xValue, currentVerticalVertexCount * Yspacing, 0);
+            vertexList.Add(vertex);
+
+            currentVerticalVertexCount++;
+        }
+
+        trianglesArray = new int[vertexList.Count - 2];
+
+        int triangleVertex = 3;
+        int breakCount = 0;
+
+        for (int i = 0; i < vertexList.Count; i++)
+        {
+            triangleList.Add(i);
+            triangleVertex--;
+
+            if (triangleVertex == 0)
+            {
+                triangleList.Add(i + 1);
+                triangleList.Add(i);
+                triangleList.Add(i - 1);
+
+                if((i + 3) > vertexList.Count)
+                {
+                    break;
+                }
+
+                i--;
+
+                triangleVertex = 3;
+            }
+        }
+
+        trianglesArray = triangleList.ToArray();
+        mesh.SetVertices(vertexList);
+        mesh.SetTriangles(trianglesArray, 0);
+        mesh.SetUVs(0, vertexList);
+
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    private void ManipulateMesh(Mesh mesh)
+    {
+        Vector3[] vertArray = mesh.vertices;
+        List<Vector2> edgeColliderPoints = new List<Vector2>();
+
+        for (int i = 0; i < vertArray.Length; i++)
+        {
+            if(vertArray[i].y != bottomPoint)
+            {
+                float x = ((vertArray[i].x + transform.position.x) / scale) + seed;
+                float y = (vertArray[i].y / scale) + seed;
+
+                float newYValue = Mathf.PerlinNoise(x, y) * height;
+                Debug.Log(string.Format("New Y Value for Index {0},{1} with Perlin Index (X):{2} (Y):{3} is {4}", vertArray[i].x, vertArray[i].y, x, y, newYValue));
+                vertArray[i].y = newYValue;
+                edgeColliderPoints.Add(new Vector2(vertArray[i].x, newYValue));
+            }
+        }
+
+        mesh.SetVertices(vertArray.ToList());
+        mesh.RecalculateNormals();
+
+        edgeCollider2D.points = edgeColliderPoints.ToArray();
+
+        Debug.Log("=============================================================");
+    }
+}
---
+++ Assets/Scripts/TerrainManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    public GameObject terrainPiecePrefab;
+
+    public List<MeshCreator> meshList = new List<MeshCreator>();
+    public MeshCreator lastMesh = null;
+    public bool spawnMesh = false;
+
+    public float xPos = 0;
+
+    public MeshProperty meshProperty;
+
+    [Header("Number Of Mesh Pieces")]
+    public int meshCount = 20;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        System.Random random = new System.Random();
+        MeshCreator.seed = random.Next(-100000, 100000);
+
+        for (int i = 0; i < meshCount; i++)
+        {
+            SpawnTerrainPiece();
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnMesh)
+        {
+            spawnMesh = false;
+            SpawnTerrainPiece();
+        }
+    }
+
+    public void SpawnTerrainPiece()
+    {
+        GameObject terrainObject;
+        MeshCreator meshCreator;
+        if (meshList.Count == 0)
+        {
+            terrainObject = Instantiate(terrainPiecePrefab, transform.position, Quaternion.identity);
+            meshCreator = terrainObject.GetComponent<MeshCreator>();
+        }
+
+        else
+        {
+            xPos += lastMesh.xValue;
+            terrainObject = Instantiate(terrainPiecePrefab, new Vector3(xPos, transform.position.y, transform.position.z), Quaternion.identity);
+            meshCreator = terrainObject.GetComponent<MeshCreator>();
+        }
+
+        //Set Mesh Properties
+        meshCreator.vertexCount = meshProperty.vertexCount;
+        meshCreator.bottomPoint = meshProperty.bottomPoint;
+        meshCreator.Xspacing = meshProperty.Xspacing;
+        meshCreator.Yspacing = meshProperty.Yspacing;
+        meshCreator.verticalVertexCount = meshProperty.verticalVertexCount;
+        meshCreator.scale = meshProperty.scale;
+        meshCreator.height = meshProperty.height;
+        meshCreator.xValue = meshProperty.xValue;
+        meshCreator.GetComponent<MeshRenderer>().material = meshProperty.material;
+
+        meshList.Add(meshCreator);
+        lastMesh = meshCreator;
+    }
+}
+
+[System.Serializable]
+public class MeshProperty
+{
+    public int vertexCount;
+    public int bottomPoint;
+    public float Xspacing;
+    public float Yspacing;
+    public int verticalVertexCount = 2;
+    public float scale;
+    public float height;
+    public float xValue;
+    public Material material;
+}
